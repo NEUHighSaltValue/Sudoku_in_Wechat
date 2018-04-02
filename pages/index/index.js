12d82844d27c8756ddec13bd1a1e7c02ed5476b7
Eric Lee <869909541@qq.com>
2018-04-02 15:47:38 +0800

    basic Sudoku page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
   //index.js
 //获取应用实例
 const app = getApp()
-
+var isButtonDown = false; 
 Page({
   data: {
     generateOk: true,
@@ -31,5 +31,60 @@ Page({
   },
   clickMe: function() {
 
-  }
+  },
+  canvasIdErrorCallback: function (e) {
+    console.error(e.detail.errMsg)
+  },
+  onReady: function (e) {
+    //使用wx.createContext获取绘图上下文context  
+    var context = wx.createCanvasContext('can1')
+    let phoneWidth = wx.getSystemInfoSync().screenWidth
+    let ratio = 750/phoneWidth
+    let boardWidth = 675
+    let lineWidth1 = 4.5
+    let lineWidth2 = 1.5
+    let cellWidth = (boardWidth - lineWidth1 * 4 - lineWidth2 * 6) / 9
+    console.log(ratio)
+    context.setStrokeStyle("#000000")
+    context.setLineWidth(lineWidth1 / ratio)
+    var startpointx = lineWidth1 / 2 / ratio
+    var startpointy = lineWidth1 / 2 / ratio
+    var tempWidth = (boardWidth - lineWidth1 * 1.5) / ratio
+    var tempHeight = (boardWidth - lineWidth1 * 1.5) / ratio
+    context.rect(startpointx, startpointy, tempWidth, tempHeight)
+    //border
+
+    startpointx = (cellWidth * 3 + lineWidth1 * 1.5 + lineWidth2 * 2) / ratio
+    tempWidth = (cellWidth * 3 + lineWidth2 * 2 + lineWidth1) / ratio
+    context.rect(startpointx, startpointy, tempWidth, tempHeight)
+    context.rect(startpointy, startpointx, tempHeight, tempWidth)
+    context.stroke()
+    //devide board into 9 parts
+
+    context.setLineWidth(lineWidth2 / ratio)
+    startpointx = (cellWidth + lineWidth1 + lineWidth2 / 2) / ratio
+    startpointy = lineWidth2 / 2
+    tempWidth = (cellWidth + lineWidth2) / ratio
+    tempHeight = (boardWidth - lineWidth2 * 1.5) / ratio
+    context.rect(startpointx, startpointy, tempWidth, tempHeight)
+    context.rect(startpointy, startpointx, tempHeight, tempWidth)
+    startpointx = (cellWidth * 4 + lineWidth1 * 2 + lineWidth2 * 2.5) / ratio
+    context.rect(startpointx, startpointy, tempWidth, tempHeight)
+    context.rect(startpointy, startpointx, tempHeight, tempWidth)
+    startpointx = (cellWidth * 7 + lineWidth1 * 3 + lineWidth2 * 4.5) / ratio
+    context.rect(startpointx, startpointy, tempWidth, tempHeight)
+    context.rect(startpointy, startpointx, tempHeight, tempWidth)
+    context.rect()
+    //devide part into 9 cells
+    context.stroke()
+    context.draw()
+  },
+  canvasStart: function (event) {
+    isButtonDown = true;
+    arrz.push(0);
+    arrx.push(event.changedTouches[0].x);
+    arry.push(event.changedTouches[0].y);
+    //context.moveTo(event.changedTouches[0].x, event.changedTouches[0].y);  
+  },  
+
 })
--- pages/sudoku/sudoku.js
+++ pages/sudoku/sudoku.js
@@ -1,9 +1,16 @@
 // pages/sudoku.js
-Page({
 
-  /**
-   * 页面的初始数据
-   */
+let phoneWidth = wx.getSystemInfoSync().screenWidth
+let boardWidthInPrx = 675
+let boardWidthInPx = boardWidthInPrx * phoneWidth / 750
+let tableWidthInPrx = 527
+let tableHeighInPrx = 213.5
+let tableWidthInPx = tableWidthInPrx * phoneWidth / 750
+let tableHeighInPx = tableHeighInPrx * phoneWidth / 750
+var selectX = -1
+var selectY = -1
+
+Page({
   data: {
     generateOk: true,
     chessboardDatas: [
@@ -17,62 +24,100 @@ Page({
       [81, 82, 83, 84, 85, 86, 87, 88, 89],
       [91, 92, 93, 94, 95, 96, 97, 98, 99],
     ]
-
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  //事件处理函数
+  bindViewTap: function () {},
+
+  onLoad: function () {},
+
+  getUserInfo: function (e) {},
+
+  clickMe: function () {},
+
+  canvasIdErrorCallback: function (e) {
+    console.error(e.detail.errMsg)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  onReady: function (e) {
+    let board = wx.createCanvasContext('board')
+    let ratio = 750 / phoneWidth
+
+    //Board
+
+    let lineWidth1 = 4.5
+    let lineWidth2 = 1.5
+    let cellWidth = (boardWidthInPrx - lineWidth1 * 4 - lineWidth2 * 6) / 9
+    //For UI designer, you can change line color here!
+    board.setStrokeStyle("#000000")
+    board.setLineWidth(lineWidth1 / ratio)
+    var startPointX = lineWidth1 / 2 / ratio
+    var startPointY = lineWidth1 / 2 / ratio
+    var tempWidth = (boardWidthInPrx - lineWidth1 * 1.5) / ratio
+    var tempHeight = (boardWidthInPrx - lineWidth1 * 1.5) / ratio
+    board.rect(startPointX, startPointY, tempWidth, tempHeight)
+    //border
+
+    startPointX = (cellWidth * 3 + lineWidth1 * 1.5 + lineWidth2 * 2) / ratio
+    tempWidth = (cellWidth * 3 + lineWidth2 * 2 + lineWidth1) / ratio
+    board.rect(startPointX, startPointY, tempWidth, tempHeight)
+    board.rect(startPointY, startPointX, tempHeight, tempWidth)
+    board.stroke()
+    //devide board into 9 parts
+
+    board.setLineWidth(lineWidth2 / ratio)
+    startPointX = (cellWidth + lineWidth1 + lineWidth2 / 2) / ratio
+    startPointY = lineWidth2 / 2
+    tempWidth = (cellWidth + lineWidth2) / ratio
+    tempHeight = (boardWidthInPrx - lineWidth2 * 3.5) / ratio
+    board.rect(startPointX, startPointY, tempWidth, tempHeight)
+    board.rect(startPointY, startPointX, tempHeight, tempWidth)
+    startPointX = (cellWidth * 4 + lineWidth1 * 2 + lineWidth2 * 2.5) / ratio
+    board.rect(startPointX, startPointY, tempWidth, tempHeight)
+    board.rect(startPointY, startPointX, tempHeight, tempWidth)
+    startPointX = (cellWidth * 7 + lineWidth1 * 3 + lineWidth2 * 4.5) / ratio
+    board.rect(startPointX, startPointY, tempWidth, tempHeight)
+    board.rect(startPointY, startPointX, tempHeight, tempWidth)
+    //devide part into 9 cells
+    board.stroke()
+    board.draw()
+
+    //!Board
+
+    //Table
+
+    startPointX = lineWidth1 / 2 / ratio
+    startPointY = lineWidth1 / 2 / ratio
+    cellWidth = (tableWidthInPrx - lineWidth1 * 6) / 5
+    tempWidth = (tableWidthInPrx - lineWidth1 * 1.5) / ratio
+    tempHeight = (tableHeighInPrx - lineWidth1 * 1.5) / ratio
+
+    let table = wx.createCanvasContext('table')
+    table.setStrokeStyle("#000000")
+    table.setLineWidth(lineWidth1 / ratio)
+    table.rect(startPointX, startPointY, tempWidth, tempHeight)
+    table.rect(startPointX, startPointY, tempWidth, tempHeight/2)
+    startPointX = (cellWidth + lineWidth1 * 1.5) / ratio
+    tempWidth = (cellWidth + lineWidth1) /ratio
+    tempHeight = (tableHeighInPrx - lineWidth1 * 1.5) / ratio
+    table.rect(startPointX, startPointY, tempWidth, tempHeight)
+    startPointX = (cellWidth * 3 + lineWidth1 * 3.5) / ratio
+    table.rect(startPointX, startPointY, tempWidth, tempHeight)
+    console.log(startPointX)
+    table.stroke()
+    table.draw()
+    //!Table
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  cellSelect: function (event) {
+    selectX = parseInt(event.changedTouches[0].x / (boardWidthInPx / 9))
+    selectY = parseInt(event.changedTouches[0].y / (boardWidthInPx / 9))
+    var xpart = parseInt(event.changedTouches[0].x / (boardWidthInPx / 9))
+    var ypart = parseInt(event.changedTouches[0].y / (boardWidthInPx / 9))
+    // console.log("X: " + event.changedTouches[0].x)
+    // console.log("Y: " + event.changedTouches[0].y) 
+    console.log("X part: " + xpart)
+    console.log("Y part: " + ypart) 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
